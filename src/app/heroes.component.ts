f0661a24c2762a963fec04711a29873a744ae629
Jaime Neves <jaimebarrosoneves@gmail.com>
2017-06-11 23:46:20 -0400

    add router

---
+++ src/app/heroes.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+@Component({
+  selector: 'my-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class HeroesComponent implements OnInit {
+  heroes:Hero[];
+  selectedHero: Hero;
+  hero: Hero = { id:1, name:'Windstorm'};
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() : void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero) : void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService
+    .getHeroes()
+    .then( heroes => this.heroes = heroes );
+  }
+
+}
